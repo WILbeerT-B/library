51083e20787da5127a664741aa6eb0cac54c5dce
WILbeerT-B <wilbertbundalian@gmail.com>
2024-09-25 21:56:06 -0700

    Add form for user input when adding a book

--- script.js
+++ script.js
@@ -4,53 +4,109 @@ const myLibrary =
     title: 'The Fellowship of the Ring', 
     author: 'J.R.R. Tolkien', 
     pages: 295, 
-    read: 'read'
+    status: 'read'
   },
   {
     title: 'The Two Towers', 
     author: 'J.R.R.', 
     pages: 195, 
-    read: 'not read'
+    status: 'not read'
   },
   {
     title: 'The Return of the King', 
     author: 'Tolkien', 
     pages: 214, 
-    read: 'not read'
+    status: 'not read'
   }
 ];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, status) {
   this.title = title;
   this. author = author;
   this.pages = pages;
-  this.read = read;
+  this.status = status;
   this.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`;
   }
 }
 
 function addBookToLibrary() {
-  let title = prompt('Please enter the title of the book: ');
-  let author = prompt('Please enter the author of the book: ');
-  let pages = prompt('Please enter number of pages: ');
-  let isRead = prompt('Have you read the book?');
-  let input = new Book(title, author, pages, isRead)
-  myLibrary.push(input);
-  displayBook();
-  return myLibrary;
+
+  // Create form
+  const form = document.createElement('form');
+
+  // Create form inputs
+  let inputTitle = document.createElement('input');
+  inputTitle.type = 'text';
+  inputTitle.name = 'title';
+  inputTitle.id = 'title';
+  inputTitle.placeholder = 'Title';
+
+  let inputAuthor = document.createElement('input');
+  inputAuthor.type = 'text';
+  inputAuthor.name = 'author';
+  inputAuthor.id = 'author';
+  inputAuthor.placeholder = 'Author';
+
+  let inputPages = document.createElement('input');
+  inputPages.type = 'text';
+  inputPages.name = 'pages';
+  inputPages.id = 'pages';
+  inputPages.placeholder = 'Pages';
+
+  let inputStatus = document.createElement('input');
+  inputStatus.type = 'text';
+  inputStatus.name = 'status';
+  inputStatus.id = 'status';
+  inputStatus.placeholder = 'Status';
+
+  // Create submit button
+  const buttonSubmit = document.createElement('button');
+  buttonSubmit.type = 'submit';
+  // buttonSubmit.value = 'Submit';
+  buttonSubmit.textContent = 'Submit';
+
+  // Add elements to form
+  form.appendChild(inputTitle);
+  form.appendChild(inputAuthor);
+  form.appendChild(inputPages);
+  form.appendChild(inputStatus);
+  form.appendChild(buttonSubmit);
+
+  document.body.appendChild(form);
+
+  buttonSubmit.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#title');
+    const author = document.querySelector('#author');
+    const pages = document.querySelector('#pages');
+    const status = document.querySelector('#status');
+
+    const myTitle = title.value;
+    title.value = '';
+    const myAuthor = author.value;
+    author.value = '';
+    const myPages = pages.value;
+    pages.value = '';
+    const myStatus = status.value;
+    status.value = '';
+    
+    let input = new Book(myTitle, myAuthor, myPages, myStatus);
+    myLibrary.push(input);
+    displayBook();
+    return myLibrary;
+  });
+  
 }
 
 function displayBook() {
+  const output = document.querySelector('.output');
+  output.textContent = "";
   for (let i=0; i<myLibrary.length; i++) {
-    const output = document.querySelector('.output');
-    output.textContent = "";
-    for (let j=0; j<4; j++) {
-      
-      const para = document.createElement("p");
-      para.textContent = myLibrary[j].title +" by "+ myLibrary[j].author + ", " + myLibrary[j].pages + " pages, " + myLibrary[j].read;
-      output.appendChild(para);
-    }
+    const para = document.createElement("p");
+    para.textContent = myLibrary[i].title +" by "+ myLibrary[i].author + ", " + myLibrary[i].pages + " pages, " + myLibrary[i].status;
+    output.appendChild(para);
   }    
 }
 
